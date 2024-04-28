079abadc9901957dffd9819cee800d5352153e87
Ian Tauro <sushruth127@gmail.com>
2024-04-28 17:54:04 +0530

    Added mongoose

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:" + PORT + "/");
-})
+});
