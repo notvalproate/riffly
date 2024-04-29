7e4c3136e9371ae04542d06bf1f9b91ed32c848f
Ian Tauro <sushruth127@gmail.com>
2024-04-29 17:18:41 +0530

    Added login component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+
 }
