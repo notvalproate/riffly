995f68dfe79380fd97ca3b2d878fe5473630b168
Ian Tauro <sushruth127@gmail.com>
2024-04-30 20:29:43 +0530

    Moved all spotify auth logic to seperate module

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const querystring = require("querystring");
-require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -11,65 +9,21 @@ app.use((req, res, next) => {
     next();
 });
 
-const clientID = process.env.SPOTIFY_CLIENT_ID;
-const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
-const redirectURI = 'http://localhost:4000/callback';
+const { clientURL, apiURL, generateSpotifyAuthLink, getAuthInfo } = require('./spotifyAuth.js');
 
 app.get('/login', (req, res) => {
-    const state = generateRandomString(128);
-    const scope = 'user-read-private user-read-email';
-
-    const params = new URLSearchParams({
-        response_type: 'code',
-        client_id: clientID,
-        scope: scope,
-        redirect_uri: redirectURI,
-        state: state
-    });
-
-    res.send({ url: 'https://accounts.spotify.com/authorize?' + params.toString() });
+    res.send({ url: generateSpotifyAuthLink() });
 });
 
 app.get('/callback', async (req, res) => {
     if(req.query.error) {
-        res.redirect('http://localhost:4200');
+        res.redirect(clientURL);
     } else {
-        const accessToken = await getAccessToken(req.query.code, req.query.state);
-        res.redirect(`http://localhost:4200/?authToken=${accessToken}`);
+        const authInfo = await getAuthInfo(req.query.code, req.query.state);
+        res.redirect(clientURL + `/?authToken=${authInfo.access_token}`);
     }
 })
 
 app.listen(PORT, () => {
     console.log("Server running on http://localhost:" + PORT + "/");
 });
-
-function generateRandomString(length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-  
-    return text;
-}
-
-async function getAccessToken(code, verifier) {
-    const params = new URLSearchParams({
-        client_id: clientID,
-        client_secret: clientSecret,
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: "http://localhost:4000/callback",
-        code_verifier: verifier,
-    });
-
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
-    });
-
-    const { access_token } = await result.json();
-    return access_token;
-}
---
+++ server/spotifyAuth.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+
+const clientID = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const clientURL = 'http://localhost:4200'
+
+const apiPORT = process.env.PORT || 4000;
+const apiURL = 'http://localhost:' + apiPORT;
+
+const redirectURI = apiURL + '/callback';
+
+function generateSpotifyAuthLink() {
+    const state = generateRandomString(128);
+    const scope = 'user-read-private user-read-email';
+
+    const params = new URLSearchParams({
+        response_type: 'code',
+        client_id: clientID,
+        scope: scope,
+        redirect_uri: redirectURI,
+        state: state
+    });
+
+    return 'https://accounts.spotify.com/authorize?' + params.toString();
+}
+
+async function getAuthInfo(code, verifier) {
+    const params = new URLSearchParams({
+        client_id: clientID,
+        client_secret: clientSecret,
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: apiURL + '/callback',
+        code_verifier: verifier,
+    });
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    return await result.json();
+}
+
+// UTILITY
+
+function generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+  
+    return text;
+}
+
+module.exports = {
+    clientURL,
+    apiURL,
+    generateSpotifyAuthLink,
+    getAuthInfo,
+};
