478278e694dfd996f9d0e7afd798053e57a1bfe6
Ian Tauro <sushruth127@gmail.com>
2024-05-02 19:20:57 +0530

    added a response status handler in spotifyapi

--- server/spotify/spotifyApi.js
+++ server/spotify/spotifyApi.js
@@ -6,13 +6,24 @@ class SpotifyAPI {
             method: "GET", headers: { Authorization: `Bearer ${req.cookies.authToken}` }
         });
 
+
+        return await this.handleResponseStatus(result, res);
+    }
+
+    static async handleResponseStatus(result, res) {
         res.status(result.status);
 
         if(result.status === 204) {
             return {};
         }
 
-        return await result.json();
+        const json = await result.json();
+
+        if(result.status >= 400 && result.status <= 429) {
+            return json.error;
+        } 
+
+        return json;
     }
 }
 
