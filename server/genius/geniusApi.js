25ac2ecd9dcecca76c57e556056183f3d5d1e9b1
Ian Tauro <sushruth127@gmail.com>
2024-05-04 10:48:34 +0530

    Pass system working

--- server/genius/geniusApi.js
+++ server/genius/geniusApi.js
@@ -10,20 +10,27 @@ class GeniusAPI {
             const searches = await this.geniusClient.songs.search(`${artists.map((artist) => artist.name).join(' ')} ${title}`);
 
             if(searches.length === 0) {
+                console.log("No Hits");
                 return null;
             }
+
+            console.log("Got searches");
     
             const topSearch = this.getAccurateResult(searches, artists, title);
             
             if(topSearch === null) {
+                console.log("Result was null!");
                 return null;
             }
+
+            console.log("Got accurate result");
     
             try {
                 const lyrics = await topSearch.lyrics();
+                console.log("Got lyrics");
                 return lyrics;
             } catch (err) {
-                console.log(err);
+                console.log('No Lyrics');
                 return null;
             }
         } catch (err) {
@@ -33,28 +40,74 @@ class GeniusAPI {
     }
 
     static getAccurateResult(searches, reqArtists, reqTitle) {
-        for(let item of searches) {
-            let hasTitle = simplifyText(item.title).includes(simplifyText(reqTitle));
-            let hasArtists = false;
+        // FIRST PASS, EQUALS
+        console.log("########################################### FIRST PASS ###########################################");
 
+        let i = 1;
+
+        for(let item of searches) {
+            item.hasArtists = false;
+            
+            console.log(`\n\n${i++}>\n`);
+            
             for(let reqArtist of reqArtists) {
+                console.log(`Required Artist: ${reqArtist.name} || Current: ${item.artist.name}`);
                 if(simplifyText(item.artist.name).includes(simplifyText(reqArtist.name))) {
-                    hasArtists = true;
+                    console.log("ABOVE HAS ARTIST");
+                    item.hasArtists = true;
                     break;
                 }
             }
 
-            if(hasTitle && hasArtists) {
+            if(!item.hasArtists) {
+                continue;
+            }
+            
+            let titleMatches = ( simplifyText(item.title) == simplifyText(reqTitle) );
+
+            console.log(`Required Title: ${simplifyText(item.title)} || Current: ${simplifyText(reqTitle)}`);
+            console.log(`|${simplifyText(item.title)}|${simplifyText(reqTitle)}|`);
+
+            if(titleMatches) {
+                console.log('ABOVE IS TITLE');
                 return item;
             }
         }
 
+        i = 1;
+
+        // SECOND PASS, INCLUDES
+        console.log("########################################### SECOND PASS ###########################################");
+
+        for(let item of searches) {
+            if(!item.hasArtists) {
+                continue;
+            }
+
+            let includesTitle = simplifyText(item.title).includes(simplifyText(reqTitle));
+
+            console.log(`\n\n${i++}>\n`);
+            console.log(`Required Title: ${reqTitle} || Current: ${item.title}`);
+
+            if(includesTitle) {
+                console.log('ABOVE HAS TITLE');
+                return item;
+            }
+        }
+
+        console.log("################# NO RESULT #################");
+
         return null;
+
+        // TEST REMIXES AND TEST LONG ONES LIKE (FROM THE SPIDERMAN MOVIE ETC)
     }
 };
 
 function simplifyText(text) {
-    return text.toLowerCase().replaceAll(' ', '');
+    return text
+        .toLowerCase()
+        .replaceAll(' ', '')
+        .replaceAll('’', "'");
 }
 
 module.exports = { 
