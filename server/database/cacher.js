cbd0e223ab77475a9ca2dd90aeab5785e3371a89
Ian Tauro <sushruth127@gmail.com>
2024-05-20 19:01:33 +0530

    updated redis cache to use localhost on production mode

--- server/database/cacher.js
+++ server/database/cacher.js
@@ -1,12 +1,19 @@
 const { createClient } = require('redis');
+require('dotenv').config();
 
-const client = createClient({
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-    },
-});
+let client = null;
+
+if(process.env.MODE === 'development') {
+    client = createClient({
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+            host: process.env.REDIS_HOST,
+            port: process.env.REDIS_PORT,
+        },
+    });
+} else {
+    client = createClient();
+}
 
 client.connect().then(() => {
     console.log("Successfully connected to redis");
