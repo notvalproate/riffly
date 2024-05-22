f986636e9eb568d2997498a800971b3a73e82976
Ian Tauro <sushruth127@gmail.com>
2024-05-22 14:17:48 +0530

    Hotfix: cache wasnt merged properly

--- server/routes/track.js
+++ server/routes/track.js
@@ -1,16 +1,8 @@
 const router = require('express').Router();
-const { SpotifyAPI } = require('../spotify/spotifyApi.js');
-const { GeniusAPI } = require('../lyrics/geniusApi.js');
-const { MusixmatchAPI } = require('../lyrics/musixmatchApi.js');
+const { LyricsAPI } =  require('../lyrics/lyricsApi.js');
 
 router.get('/lyrics', async (req, res) => {
-    const currentTrack = await SpotifyAPI.getSongByISRC(req.query.isrc, req, res);
-
-    let lyrics = await GeniusAPI.getLyrics(currentTrack.artists, currentTrack.title);
-
-    if(lyrics === null) {
-        lyrics = await MusixmatchAPI.getLyrics(req.query.isrc);
-    }
+    const lyrics = await LyricsAPI.getLyrics(req, res);
 
     res.json(lyrics);
 });
