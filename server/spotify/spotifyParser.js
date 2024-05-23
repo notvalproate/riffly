b8ebaf699a48a213705ec9f63419c4a1fd69e2ca
Ian Tauro <sushruth127@gmail.com>
2024-05-23 20:41:50 +0530

    Backend handles the player having episode

--- server/spotify/spotifyParser.js
+++ server/spotify/spotifyParser.js
@@ -17,6 +17,16 @@ class SpotifyParser {
     }
 
     static parsePlayerInfo(playerInfo) {
+        if(playerInfo.currently_playing_type === 'track') {
+            return this.parseTrackInfo(playerInfo);
+        } else {
+            return this.parseEpisodeInfo(playerInfo);
+        }
+    }
+
+    static parseTrackInfo(playerInfo) {
+        console.log(playerInfo.item);
+
         let parsed = {
             player: {
                 playing: playerInfo.is_playing,
@@ -38,7 +48,36 @@ class SpotifyParser {
                 url: playerInfo.item.external_urls.spotify,
                 isrc: playerInfo.item.external_ids.isrc,
                 popularity: playerInfo.item.popularity,
-            }
+            },
+        };
+
+        return parsed;
+    }
+
+    static parseEpisodeInfo(playerInfo) {
+        console.log(playerInfo.item);
+
+        let parsed = {
+            player: {
+                playing: playerInfo.is_playing,
+                progress: playerInfo.progress_ms,
+                duration: playerInfo.item.duration_ms,
+                device: playerInfo.device.name,
+            },
+            itemType: playerInfo.currently_playing_type,
+            item: {
+                title: playerInfo.item.name,
+                show: {
+                    name: playerInfo.item.show.name,
+                    url: playerInfo.item.show.external_urls.spotify,
+                },
+                images: {
+                    default: playerInfo.item.images[1].url,
+                    large: playerInfo.item.images[0].url,
+                },
+                url: playerInfo.item.external_urls.spotify,
+                id: playerInfo.item.id,
+            },
         };
 
         return parsed;
