f5ae8879702207ae7ca3afd6e3066bcd417e5fcb
Ian Tauro <sushruth127@gmail.com>
2024-05-24 13:14:30 +0530

    Fixed some pathings

--- server/routes/track.js
+++ server/routes/track.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { LyricsAPI } =  require('../lyrics/lyricsApi.js');
+const { LyricsAPI } =  require('../controllers/lyrics/lyricsApi.js');
 
 router.get('/lyrics', async (req, res) => {
     const lyrics = await LyricsAPI.getLyrics(req, res);
