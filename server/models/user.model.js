46ddabd81d866df7b2fa205f2c0623dbb389da2a
Ian Tauro <sushruth127@gmail.com>
2024-06-07 20:57:58 +0530

    Added initial model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,2 +1,89 @@
 import dynamoose from 'dynamoose';
 
+const userSchema = new dynamoose.Schema({
+    id: {
+        type: String,
+        hashKey: true,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: Object,
+        schema: {
+            hidden: {
+                type: Boolean,
+            },
+            online: {
+                type: Boolean,
+            },
+        },
+        get: (value) => {
+            return {
+                online: value.hidden ? false : value.online,
+            }
+        },
+        default: {
+            hidden: false,
+            online: false,
+        },
+    },
+    friends: {
+        type: Object,
+        schema: {
+            list: {
+                type: Set,
+                schema: [{
+                    type: Object,
+                    schema: {
+                        id: {
+                            type: String,
+                            required: true,
+                        },
+                    },
+                }],
+                required: true,
+            },
+            requests: {
+                type: Set,
+                schema: [{
+                    type: Object,
+                    schema: {
+                        id: {
+                            type: String,
+                            required: true,
+                        },
+                    },
+                }],
+                required: true,
+            },
+        },
+        default: {
+            list: [],
+            requests: [],
+        },
+    },
+    recommendations: {
+        type: Set,
+        schema: [{
+            type: Object,
+            schema: {
+                id: {
+                    type: String,
+                    required: true,
+                },
+                songId: {
+                    type: String,
+                    required: true,
+                },
+            },
+        }],
+        default: [],
+    },
+}, {
+    timestamps: true,
+});
+
+export default dynamoose.model('User', userSchema);
