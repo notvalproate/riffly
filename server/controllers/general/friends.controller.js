0ad542b1c96d820438e1fcc72aa58cabead1ccb4
Ian Tauro <sushruth127@gmail.com>
2024-06-09 08:39:24 +0530

    Added method to get friends list and requests

--- server/controllers/general/friends.controller.js
+++ server/controllers/general/friends.controller.js
@@ -1,14 +1,47 @@
 import asyncHandler from 'express-async-handler';
+import SpotifyParser from '../../utils/spotify.parser.js';
 import { spotifyFetch } from "../spotify/spotifyApi.js"
+import User from "../../models/user.model.js"
 
 export default class Friends {
     static getList = asyncHandler(async (req, res) => {
-        
+        let userInfo = await spotifyFetch('GET', '/me', req);
+
+        const user = await User.get(userInfo.id);
+
+        if (!user) {
+            throw new ApiError(404, 'User not found, Mostly an error in registeration, Please re-login and try again!');
+        }
+
+        const friends = await getUsersBatch(user.friends.list);
+
+        res.status(200).json({ friendsList: friends});
     });
 
     static getRequests = asyncHandler(async (req, res) => {
-        
+        let userInfo = await spotifyFetch('GET', '/me', req);
+
+        const user = await User.get(userInfo.id);
+
+        if (!user) {
+            throw new ApiError(404, 'User not found, Mostly an error in registeration, Please re-login and try again!');
+        }
+
+        const pendingRequests = await getUsersBatch(user.friends.requests);
+
+        res.status(200).json({ pendingRequests: pendingRequests});
     });
 };
+
+async function getUsersBatch(users) {
+    const parsedUsers = [];
+
+    users.forEach(async (user) => {
+        const userInfo = spotifyFetch('GET', `/users/${user.id}`, req);
+        parsedUsers.push(userInfo);
+    });
+
+    return Promise.all(parsedUsers.map(async user => SpotifyParser.parseFriendInfo(await user)));
+}
 
 Object.freeze(Friends);
