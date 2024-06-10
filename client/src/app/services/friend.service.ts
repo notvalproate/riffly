5eeb881047422a95ea63d2e8a19f9c75b7b00de3
Ian Tauro <sushruth127@gmail.com>
2024-06-10 23:16:32 +0530

    Added friend service to interact with friends endpoint

---
+++ client/src/app/services/friend.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendService extends ApiService {
+    getAllFriends() {
+        return this.http.get(this.apiUrl + '/me/friends', this.requestOptions);
+    }
+
+    removeFriend(friendId: string) {
+        const params = new URLSearchParams({
+            id: friendId,
+        });
+
+        return this.http.delete(this.apiUrl + '/me/friends/list?' + params.toString(), this.requestOptions);
+    }
+
+    cancelPending(requestedId: string) {
+        const params = new URLSearchParams({
+            id: requestedId,
+        });
+
+        return this.http.delete(this.apiUrl + '/me/friends/pending?' + params.toString(), this.requestOptions);
+    }
+
+    sendRequest(requestedId: string) {
+        const params = new URLSearchParams({
+            id: requestedId,
+        });
+
+        return this.http.post(this.apiUrl + '/me/friends/request?' + params.toString(), this.requestOptions);
+    }
+
+    acceptRequest(requestedId: string) {
+        const params = new URLSearchParams({
+            id: requestedId,
+        });
+
+        return this.http.put(this.apiUrl + '/me/friends/request?' + params.toString(), this.requestOptions);
+    }
+
+    rejectRequest(requestedId: string) {
+        const params = new URLSearchParams({
+            id: requestedId,
+        });
+
+        return this.http.delete(this.apiUrl + '/me/friends/request?' + params.toString(), this.requestOptions);
+    }
+}
