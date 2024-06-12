db3b0e96332fa0331daa5f612ebd06fa583f4912
Ian Tauro <sushruth127@gmail.com>
2024-06-12 22:46:43 +0530

    Added input and error message area

--- client/src/app/general/friend-sidebar/add-friend/add-friend.component.ts
+++ client/src/app/general/friend-sidebar/add-friend/add-friend.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-friend',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, FormsModule],
   templateUrl: './add-friend.component.html',
   styleUrl: './add-friend.component.scss'
 })
 export class AddFriendComponent {
+    inputId : string = "";
 
+    errorOccured : boolean = true;
+    errorMessage : string = "User already exists in your friend list!";
+
+    fauserPlus = faUserPlus;
+
+    clearError() : void {
+        this.errorOccured = false;
+    }
+
+    sendFriendRequest() : void {
+        this.errorOccured = true;
+    }
 }
