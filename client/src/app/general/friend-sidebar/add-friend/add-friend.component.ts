341b135679cbed5a3160c194f1fb4b8b0b30fe9c
Ian Tauro <sushruth127@gmail.com>
2024-06-12 23:05:53 +0530

    Sending friend request working, changes to the post and put

--- client/src/app/general/friend-sidebar/add-friend/add-friend.component.ts
+++ client/src/app/general/friend-sidebar/add-friend/add-friend.component.ts
@@ -1,7 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { FormsModule } from '@angular/forms';
+
+import { FriendService } from '../services/friend.service';
 
 @Component({
   selector: 'app-add-friend',
@@ -11,18 +13,34 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-friend.component.scss'
 })
 export class AddFriendComponent {
+    friends : FriendService = inject(FriendService);
     inputId : string = "";
 
-    errorOccured : boolean = true;
-    errorMessage : string = "User already exists in your friend list!";
+    successOccured : boolean = false;
+    errorOccured : boolean = false;
+    errorMessage : string = '';
 
     fauserPlus = faUserPlus;
 
     clearError() : void {
         this.errorOccured = false;
+        this.successOccured = false;
     }
 
     sendFriendRequest() : void {
-        this.errorOccured = true;
+        if(this.errorOccured || this.successOccured) {
+            return;
+        }
+
+        this.friends.sendRequest(this.inputId).subscribe({
+            next: (resp: any) => {
+                console.log(resp);
+                this.successOccured = true;
+            },
+            error: (resp: any) => {
+                this.errorOccured = true;
+                this.errorMessage = resp.error.message;
+            },
+        });
     }
 }
