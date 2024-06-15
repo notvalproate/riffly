179f0ae4259c9fba481573f1b4fcc475a2764ec9
Ian Tauro <sushruth127@gmail.com>
2024-06-15 12:43:20 +0530

    Added dropdown and copy with cdk

--- client/src/app/general/friend-sidebar/profile/profile.component.ts
+++ client/src/app/general/friend-sidebar/profile/profile.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEllipsisVertical, faCopy } from '@fortawesome/free-solid-svg-icons';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+    faEllipsisVertical = faEllipsisVertical;
+    faCopy = faCopy;
 
+    clipboard: Clipboard = inject(Clipboard);
+
+    copyId() : void {
+        this.clipboard.copy("testcopy");
+    }
+
+    displayDropdown() : void {
+        console.log("Displaying dropdown");
+    }
 }
