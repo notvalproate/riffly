181aea78cb00b9a160384f6e6e970934be67913d
Ian Tauro <sushruth127@gmail.com>
2024-06-15 14:15:17 +0530

    Added pfp and shows initial if no pfp

--- client/src/app/general/friend-sidebar/profile/profile.component.ts
+++ client/src/app/general/friend-sidebar/profile/profile.component.ts
@@ -4,6 +4,8 @@ import { faEllipsisVertical, faCopy } from '@fortawesome/free-solid-svg-icons';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { Renderer2 } from '@angular/core';
 
+import { UserInfoService } from '../../../../shared/services/user-info.service';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
@@ -12,26 +14,49 @@ import { Renderer2 } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements OnInit {
+    private renderer: Renderer2 = inject(Renderer2);
+    private clipboard: Clipboard = inject(Clipboard);
+    private userInfoService: UserInfoService = inject(UserInfoService);
+
     faEllipsisVertical = faEllipsisVertical;
     faCopy = faCopy;
-
-    renderer: Renderer2 = inject(Renderer2);
-    clipboard: Clipboard = inject(Clipboard);
 
     dropdownClick: boolean = false;
     dropdownVisible: boolean = false;
 
+    displayName : string = '';
+    profileId : string = '';
+    profileUrl : string = '';
+    profilePictureUrl : string = '';
+
     ngOnInit(): void {
-        this.renderer.listen('window', 'click',(e : Event)=>{
+        this.renderer.listen('window', 'click', (e : Event) => {
             if(!this.dropdownClick) {
                 this.dropdownVisible = false;
             }
             this.dropdownClick = false;
        });
+
+       this.userInfoService.getUserInfo().subscribe({
+            next: (resp: any) => {
+                const info = resp.body;
+
+                this.displayName = info.user.displayName;
+                this.profileUrl = info.user.url;
+                this.profileId = info.user.id;
+
+                if(info.images) {
+                    this.profilePictureUrl = info.images.default;
+                }
+            },
+            error: (resp: any) => {
+                console.log(resp.error);
+            }
+        });
     }
 
     copyId() : void {
-        this.clipboard.copy("testcopy");
+        this.clipboard.copy(this.profileId);
     }
 
     displayDropdown() : void {
